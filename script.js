4b23c26de2d4c835e0f686aec08d055c9dea837a
Justin Xia <justinxia9@gmail.com>
2021-09-09 12:12:13 -0400

    Add function to take a photo

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const snap = document.querySelector('.snap');
 function getVideo() {
   navigator.mediaDevices.getUserMedia({video: true, audio: false}) // returns a promise
     .then(localMediaStream => {
-      console.log(localMediaStream);
       // video.src = window.URL.createObjectURL(localMediaStream); is deprecated in Chrome
       video.srcObject = localMediaStream;
       video.play();
@@ -16,5 +15,32 @@ function getVideo() {
        console.error("OH NO!!!", err);
      });
 }
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
+}
+
+function takePhoto() {
+  // play sound
+  snap.currentTime = 0;
+  snap.play();
+
+  // take data out of canvas
+  const data = canvas.toDataURL("image/jpeg");
+  const link = document.createElement("a");
+  link.href = data;
+  link.setAttribute("download", "handsome");
+  link.innerHTML = `<img src="${data}" alt = "Handsome man" />`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+video.addEventListener("canplay", paintToCanvas);
 
 getVideo();
